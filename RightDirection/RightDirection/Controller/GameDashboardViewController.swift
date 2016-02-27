6bc2f5246a60dfabdf76b9bd0893abf28a3776b1
Mike Mikina <mike@mikina.pl>
2016-02-27 15:38:28 +0100

    Root, game and dashboard VC init.

---
+++ RightDirection/RightDirection/Controller/GameDashboardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameDashboardViewController.swift
+//  RightDirection
+//
+//  Created by Mike Mikina on 2/27/16.
+//  Copyright © 2016 FDT. All rights reserved.
+//
+
+import UIKit
+
+class GameDashboardViewController: RootViewController {
+
+  override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+  
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      // Get the new view controller using segue.destinationViewController.
+      // Pass the selected object to the new view controller.
+  }
+  */
+
+}
