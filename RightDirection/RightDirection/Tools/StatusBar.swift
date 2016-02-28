ed91b4dfc37f6acf1b2704bfb91dd00cb2eb0a69
Mike Mikina <mike@mikina.pl>
2016-02-28 23:18:15 +0100

    Status bar refactor.

---
+++ RightDirection/RightDirection/Tools/StatusBar.swift
@@ -0,0 +1,15 @@
+//
+//  StatusBar.swift
+//  RightDirection
+//
+//  Created by Mike Mikina on 2/28/16.
+//  Copyright © 2016 FDT. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+  override public func preferredStatusBarStyle() -> UIStatusBarStyle {
+    return .LightContent
+  }
+}
