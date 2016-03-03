7776cc235e5c31d4ef7cf0e7727362b594b17703
Mike Mikina <mike@mikina.pl>
2016-03-03 20:13:57 +0100

    Directions view mock added. Random position and size of directions view has been implemented.

--- RightDirection/RightDirection/Controller/GameBoardViewController.swift
+++ RightDirection/RightDirection/Controller/GameBoardViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class GameBoardViewController: UIViewController {
 
+  @IBOutlet weak var directionsViewHeight: NSLayoutConstraint!
+  @IBOutlet weak var directionsViewWidth: NSLayoutConstraint!
+  @IBOutlet weak var directionsViewAxisX: NSLayoutConstraint!
+  @IBOutlet weak var directionsViewAxisY: NSLayoutConstraint!
+  
+  @IBOutlet weak var directionsView: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    print("didload width: \(self.view.frame.size.width) height: \(self.view.frame.size.height)")
 
     self.setupView()
+  }
+    
+  override func viewWillAppear(animated: Bool) {
+    print("willappear width: \(self.view.frame.size.width) height: \(self.view.frame.size.height)")
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    print("didappear width: \(self.view.frame.size.width) height: \(self.view.frame.size.height)")
   }
   
   func setupView() {
@@ -30,6 +47,22 @@ class GameBoardViewController: UIViewController {
   
   func handleSwipe(swipe: UISwipeGestureRecognizer) {
     print("swipe: \(swipe.direction)")
+    let randomSquareSize = Int.random(50...250)
+    
+    let maxX = Int(self.view.frame.size.width) - randomSquareSize
+    let maxY = Int(self.view.frame.size.height - 44) - randomSquareSize
+    
+    let randomX = Int.random(1...maxX)
+    let randomY = Int.random(1...maxY)
+    
+    self.directionsViewAxisX.constant = CGFloat(randomX)
+    self.directionsViewAxisY.constant = CGFloat(randomY)
+    
+    self.directionsViewWidth.constant = CGFloat(randomSquareSize)
+    self.directionsViewHeight.constant = CGFloat(randomSquareSize)
+    
+    
+        print("width: \(self.view.frame.size.width) height: \(self.view.frame.size.height)")
   }
 
   override func didReceiveMemoryWarning() {
