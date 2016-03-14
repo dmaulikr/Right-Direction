e348b818d09a0231d0cf47e45edce90bd38a95ac
Mike Mikina <mike@mikina.pl>
2016-03-14 07:56:19 +0100

    Show messages implementation.

--- RightDirection/RightDirection/View/MessageView.swift
+++ RightDirection/RightDirection/View/MessageView.swift
@@ -11,16 +11,41 @@ import UIKit
 class MessageView: UIView {
   
   @IBOutlet weak var messageLabel: UILabel!
-  let messageViewWidth = 200
-  let messageViewHeight = 150
+  let messageViewWidth = 220
+  let messageViewHeight = 120
   let messageRoundedCorners = 30
   var completion: (() -> ())?
   var messages = [String]()
   
   // Simple view that shows messages from array with delay, after finish it will 
   // fire completion closure
-  func showMessages(messages: [String]) {
-    
-  }
+  func showMessages(var messages: [String]) {
+    if messages.count > 0 {
+      var currentText = ""
 
+      for (index, msg) in messages.enumerate() {
+        if index == 0 {
+          currentText = msg
+          messages.removeFirst()
+        }
+      }
+      
+      UIView.animateWithDuration(1, delay: 0, options: [.TransitionNone], animations: { () -> Void in
+        self.messageLabel.text = currentText
+        self.messageLabel.alpha = 1
+        self.messageLabel.alpha = 0
+        }, completion: { (complete) -> Void in
+          if complete {
+            if messages.count > 0 {
+              self.showMessages(messages)
+            }
+            else {
+              if let finish = self.completion {
+                finish()
+              }
+            }
+          }
+      })
+    }
+  }
 }
